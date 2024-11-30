c1a0c30ddca9a924550bd12047e598748a187b3a
haaydenjones <hwjones90713@gmail.com>
2024-11-30 03:08:50 -0800

    Map generation creation
    List of changes:
    - Uploaded shapefiles for Calfornia Counties to Resource folders
    - Wrote map.py; program that takes map shapefiles and converts them to readable JSON for drawing boundaries of CA Counties

---
+++ map.py
@@ -0,0 +1,27 @@
+import geopandas
+import json
+
+def convert_map_to_json(shape_path, save_path):
+    # Read shapefile into DataFrame; convert DF to JSON
+    counties = geopandas.read_file(shape_path)
+    json_data = counties.to_json()
+
+    # Convert JSON to dictionary for iteration
+    json_dict = json.loads(json_data)
+    
+    # Set up initial values for storm counts
+    for storm in json_dict['features']:
+        storm['properties']['rainstorm_count'] = 0
+        storm['properties']['hailstorm_count'] = 0
+        storm['properties']['hurricane_count'] = 0  # hurricanes in cali, lol
+        
+    # Save the updated JSON to a file
+    with open(save_path, 'w') as outfile:
+        json.dump(json_dict, outfile)
+
+    print(f'Save successful! JSON of counties stored at: {save_path}')
+
+if __name__ == "__main__":
+    shape_path = "Resources/Resource Shapes/CA_Counties.shp"  # Adjust this path
+    save_path = "Resources/CA_Counties.json"  # Save path for the updated JSON
+    convert_map_to_json(shape_path, save_path)
